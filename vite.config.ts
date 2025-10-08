f551461a2542f6ff1af92e5eb5574c7997f223ac
memsbdm <mehmetbdm@outlook.fr>
2025-10-08 22:42:35 +0400

    chore: setup unocss and reset.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import { getDirname } from '@adonisjs/core/helpers'
-import inertia from '@adonisjs/inertia/client'
-import react from '@vitejs/plugin-react'
-import adonisjs from '@adonisjs/vite/client'
+import Inertia from '@adonisjs/inertia/client'
+import React from '@vitejs/plugin-react'
+import Adonisjs from '@adonisjs/vite/client'
+import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
-  plugins: [inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.tsx' } }), react(), adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] })],
+  plugins: [
+    Inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.tsx' } }),
+    React(),
+    Adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] }),
+    UnoCSS(),
+  ],
 
   /**
    * Define aliases for importing modules from
