cc7b02a8287f05f156f4236c57c22593827dae5b
memsbdm <mehmetbdm@outlook.fr>
2025-10-10 20:56:54 +0400

    fix(design-system:dynamic-island): fix transparency

--- packages/design-system/src/molecules/dynamic-island/dynamic_island.tsx
+++ packages/design-system/src/molecules/dynamic-island/dynamic_island.tsx
@@ -7,20 +7,21 @@ export const DynamicIsland = () => {
 
   return (
     <>
-      <div className="glass h-10 w-full flex items-center justify-between rounded-3xl">
-        <div className="flex items-center gap-3">
-          <div className="ml-4">
-            <Available />
+      <div className="relative w-full">
+        <div className="glass h-10 flex items-center justify-between rounded-3xl">
+          <div className="flex items-center gap-3">
+            <div className="ml-4">
+              <Available />
+            </div>
+            <span>available right now for your project :)</span>
           </div>
-          <span>available right now for your project :)</span>
+          <button
+            className="glass h-[1.35rem] w-[5.5rem] flex items-center justify-center mr-3 rounded-xl bg-[rgba(0,0,0,0.9)]"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {isOpen ? 'close' : 'contact'}
+          </button>
         </div>
-        <button
-          className="glass h-[1.35rem] w-[5.5rem] flex items-center justify-center mr-3 rounded-xl bg-[rgba(0,0,0,0.9)]"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {isOpen ? 'close' : 'contact'}
-        </button>
-
         <DynamicIslandModal isOpen={isOpen} />
       </div>
     </>
