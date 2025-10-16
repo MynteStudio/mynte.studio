a2d6cedcf82c818a802d3af8ffab34400c89e8af
memsbdm <mehmetbdm@outlook.fr>
2025-10-16 17:26:14 +0400

    chore: remove boilerplate code

--- inertia/app/app.ts
+++ inertia/app/app.ts
@@ -1,30 +1,26 @@
 /// <reference path="../../adonisrc.ts" />
 /// <reference path="../../config/inertia.ts" />
 
-import '../css/app.css';
+import '../css/app.css'
 import { createSSRApp, h } from 'vue'
 import type { DefineComponent } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
 
-const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
-
 createInertiaApp({
-  progress: { color: '#5468FF' },
+  progress: { color: '#ffffff' },
 
-  title: (title) => `${title} - ${appName}`,
+  title: (title) => `${title} - Mynte Studio`,
 
   resolve: (name) => {
     return resolvePageComponent(
       `../pages/${name}.vue`,
-      import.meta.glob<DefineComponent>('../pages/**/*.vue'),
+      import.meta.glob<DefineComponent>('../pages/**/*.vue')
     )
   },
 
   setup({ el, App, props, plugin }) {
-    
     createSSRApp({ render: () => h(App, props) })
-    
       .use(plugin)
       .mount(el)
   },
